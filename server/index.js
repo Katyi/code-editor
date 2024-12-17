dacca4cf3696cc6bbdb96f31757d7b1c3ba5f59f
Alexandra Egorova <egorova.aleksandra@gmail.com>
2024-12-17 16:02:24 +0200

    fixed CORS for versel api again

--- server/index.js
+++ server/index.js
@@ -13,15 +13,17 @@ app.use(bodyParser.json());
 
 //cors
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
   res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader(
     'Access-Control-Allow-Methods',
-    'GET, POST, PUT, PATCH, OPTIONS, DELETE'
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   );
   next();
 });
